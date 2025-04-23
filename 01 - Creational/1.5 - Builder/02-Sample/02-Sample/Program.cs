6a075a0548c96660c75b62620297f94a49e86d54
Paulo Pitte <pauloanalista@outlook.com>
2025-04-23 19:07:05 -0300

    Builder Sample 2 estudent

---
+++ 01 - Creational/1.5 - Builder/02-Sample/02-Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Builder Sample 2");
+
+
+public sealed partial class OrgChartMember
+{
+    public OrgChartMember(string id, string name) => (Id, Name) = (id, name);
+
+    public string Name { get; private set; }
+    public string Id { get; private set; }
+    Lazy<List<OrgChartMember>> _team = new();
+    public IReadOnlyList<OrgChartMember> Team => _team.Value;
+    public static OrgChartMember Create(string id, string name) 
+        => new(id, name);
+}
+
+sealed partial class OrgChartMember
+{
+    public OrgChartMember AddTeamMember(string id, string name)
+    {
+        var member = new OrgChartMember(id, name);
+        _team.Value.Add(member);
+        return member;
+    }
+}
