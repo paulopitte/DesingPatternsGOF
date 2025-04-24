e810c478427a728b9f0f33f09706359e6f2ff562
Paulo Pitte <pauloanalista@outlook.com>
2025-04-24 10:44:21 -0300

    retactor in Build Sample 2

--- 01 - Creational/1.5 - Builder/02-Sample/02-Sample/Program.cs
+++ 01 - Creational/1.5 - Builder/02-Sample/02-Sample/Program.cs
@@ -1,24 +1,61 @@
 ﻿Console.WriteLine("Builder Sample 2");
 
+OrgChartMember builder = OrgChartMember.Create("1", "Paulo Pitte")
+                  .AddSubordinate("2", "Fernanda", b => b
+                    .AddSubordinate("2-1", "Benjamin"))
+                    .AddSubordinate("2-2", "Jose Paulo", c => c
+                            .AddSubordinate("2-2-1", "Maria")
+                            .AddSubordinate("2-2-2", "João"))
+                  .AddSubordinate("3", "Ines");
+                  //.Build(); /// REALIZO A OPERAÇÃO DE CHAMADA DE FORMA IMPLICITA
 
-public sealed partial class OrgChartMember
+
+/// Classe de Representação 
+public sealed partial record class OrgChartMember
 {
-    public OrgChartMember(string id, string name) => (Id, Name) = (id, name);
+    private OrgChartMember(string id, string name) => (Id, Name) = (id, name);
 
     public string Name { get; private set; }
     public string Id { get; private set; }
     Lazy<List<OrgChartMember>> _team = new();
     public IReadOnlyList<OrgChartMember> Team => _team.Value;
-    public static OrgChartMember Create(string id, string name) 
-        => new(id, name);
-}
 
-sealed partial class OrgChartMember
-{
-    public OrgChartMember AddTeamMember(string id, string name)
+    /// Factory Method
+    public static OrgChartBuilder Create(string id, string name) => new OrgChartBuilder(new OrgChartMember(id, name));
+
+
+
+    ///Classe com lógica da construção(Builder)
+    public class OrgChartBuilder
     {
-        var member = new OrgChartMember(id, name);
-        _team.Value.Add(member);
-        return member;
+        readonly OrgChartMember _root;
+        public OrgChartBuilder(OrgChartMember root) => _root = root;
+
+
+        public OrgChartBuilder AddSubordinate(string id, string name)
+        {
+            _root._team.Value.Add(new OrgChartMember(id, name));
+            return this;
+        }
+
+        public OrgChartBuilder AddSubordinate(string id, string name, Action<OrgChartBuilder> action)
+        {
+            var a = new OrgChartBuilder(new OrgChartMember(id, name));
+            action(a);
+            _root._team.Value.Add(a.Build());
+            return this;
+        }
+
+        public OrgChartMember Build() => _root;
+
+        public static implicit operator OrgChartMember(OrgChartBuilder input) => input.Build();
+
+
+
     }
 }
+
+
+
+
+
